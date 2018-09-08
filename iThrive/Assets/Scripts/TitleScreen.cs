9f76da023ecffc9da0c0bf344285f838f61409d5
AmoghSubhedar <amogh.subhedar@gmail.com>
2018-09-08 14:25:03 -0700

    Fading Transition

--- iThrive/Assets/Scripts/TitleScreen.cs
+++ iThrive/Assets/Scripts/TitleScreen.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleScreen : MonoBehaviour {
+    public GameObject titleUI;
+    public GameObject playUI;
+    public Image blackScreen;
 
 	// Use this for initialization
 	void Start () {
-		
+
+        blackScreen.canvasRenderer.SetAlpha(0.0f);
 	}
 	
 	// Update is called once per frame
@@ -16,6 +21,9 @@ public class TitleScreen : MonoBehaviour {
 
     public void Play() {
         Debug.Log("Transition to play cutscene");
+        titleUI.SetActive(false);
+        StartCoroutine(fade(playUI));
+        //playUI.SetActive(true);
     }
     public void HTP()
     {
@@ -25,5 +33,18 @@ public class TitleScreen : MonoBehaviour {
     {
         Application.Quit();
         Debug.Break();
+    }
+    IEnumerator fade(GameObject sceneUI)
+    {
+        blackScreen.CrossFadeAlpha(1f, 2f, true);      
+        yield return new WaitForSeconds(2f);
+        blackScreen.CrossFadeAlpha(0f, 1f, true);
+        yield return new WaitForSeconds(1f);
+        sceneUI.SetActive(true);
+    }
+    public void goBack()
+    {
+        playUI.SetActive(false);
+        StartCoroutine(fade(titleUI));
     }
 }
