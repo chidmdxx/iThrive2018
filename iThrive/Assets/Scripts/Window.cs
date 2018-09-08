fb114219ef77e7de3e7849a9a738abec709aff44
Rachid Cesin <rachid@outlook.com>
2018-09-08 14:04:36 -0700

    Proper movable windows

--- iThrive/Assets/Scripts/Window.cs
+++ iThrive/Assets/Scripts/Window.cs
@@ -1,22 +1,36 @@
 ﻿using System;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public abstract class Window : MonoBehaviour {
+public abstract class Window : MonoBehaviour, IPointerDownHandler, IDragHandler
+{
 
     public bool IsCurrentWindow { get; set; }
     public Button CloseButton { get; set; }
     protected virtual Action OnStart { get; set; }
     protected virtual Action OnUpdate { get; set; }
 
-	// Use this for initialization
-	void Start ()
+    private bool movable;
+    private Vector2 pointerOffset;
+    private RectTransform canvasRectTransform;
+    private RectTransform panelRectTransform;
+
+    // Use this for initialization
+    void Start ()
     {
         this.OnStart();
         var buttons = this.GetComponentsInChildren<Button>();
         this.CloseButton = buttons.First(field => field.name.Equals("CloseButton"));
         this.CloseButton.onClick.AddListener(Close);
+
+        var canvas = GetComponentInParent<Canvas>();
+        if (canvas != null)
+        {
+            canvasRectTransform = canvas.transform as RectTransform;
+            panelRectTransform = transform as RectTransform;
+        }
     }
 	
 	// Update is called once per frame
@@ -46,5 +60,40 @@ public abstract class Window : MonoBehaviour {
     public void Close()
     {
         UnityEngine.Object.Destroy(this.gameObject);
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        this.movable = data.pointerCurrentRaycast.gameObject.name.Equals("DragZone");
+        this.panelRectTransform.SetAsLastSibling();
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(panelRectTransform, data.position, data.pressEventCamera, out pointerOffset);
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        if (this.movable && panelRectTransform != null)
+        {
+            var pointerPostion = this.ClampToWindow(data);
+
+            Vector2 localPointerPosition;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRectTransform, pointerPostion, data.pressEventCamera, out localPointerPosition))
+            {
+                this.panelRectTransform.localPosition = localPointerPosition - pointerOffset;
+            }
+        }
+    }
+
+    private Vector2 ClampToWindow(PointerEventData data)
+    {
+        var rawPointerPosition = data.position;
+
+        var canvasCorners = new Vector3[4];
+        this.canvasRectTransform.GetWorldCorners(canvasCorners);
+
+        var clampedX = Mathf.Clamp(rawPointerPosition.x, canvasCorners[0].x, canvasCorners[2].x);
+        var clampedY = Mathf.Clamp(rawPointerPosition.y, canvasCorners[0].y, canvasCorners[2].y);
+
+        var newPointerPosition = new Vector2(clampedX, clampedY);
+        return newPointerPosition;
     }
 }
