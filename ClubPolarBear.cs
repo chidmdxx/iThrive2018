3a3a10ecfc67a02970abd1268d1f1d0f5efe5c2a
JungDefiant <42555252+JungDefiant@users.noreply.github.com>
2018-09-08 22:58:01 -0700

    Add files via upload

---
+++ ClubPolarBear.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClubPolarBear : MonoBehaviour {
+
+    public InternetWindow InternetWindow;
+    public float timePlayed;
+
+	// Update is called once per frame
+	void Update () {
+        if (InternetWindow.IsCurrentWindow) return; //send message to player object -> trigger energy modification based on time played
+        timePlayed += Time.deltaTime;
+	}
+}
