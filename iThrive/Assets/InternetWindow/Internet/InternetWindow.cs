a0ff84ed636bf1fd1c3138d4382b36b714071171
JungDefiant <42555252+JungDefiant@users.noreply.github.com>
2018-09-09 16:14:28 -0700

    Add files via upload

---
+++ iThrive/Assets/InternetWindow/Internet/InternetWindow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InternetWindow : Window {
+
+    public GameObject homePage;
+    public GameObject loadingPage;
+    public GameObject header;
+    public Text URLText;
+    GameObject currentPage;
+
+	public void NavigateToPage(GameObject newPage)
+    {
+        currentPage.SetActive(false);
+        currentPage = newPage;
+        currentPage.SetActive(true);
+    }
+
+    public void OnEnable()
+    {
+        currentPage = homePage;
+        currentPage.SetActive(true);
+        header.SetActive(true);
+    }
+
+    public void OnDisable()
+    {
+        gameObject.SetActive(false);
+    }
+}
