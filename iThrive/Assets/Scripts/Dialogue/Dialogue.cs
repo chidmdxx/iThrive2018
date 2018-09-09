e1c29a9f0491da7b3d4dc3a8c1a145e2adadeca5
Zapata Ruiz <manruiz@amazon.com>
2018-09-08 21:30:09 -0700

    Dialogue stuff

---
+++ iThrive/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue {
+   
+    [TextArea(1, 5)]
+    public string[] sentences;
+
+}
---
+++ iThrive/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public Text dialogueText;
+
+    private Queue<string> sentenceQueue;
+
+    void Start()
+    {
+        sentenceQueue = new Queue<string>();
+        sentenceQueue.Enqueue("Hola");
+        sentenceQueue.Enqueue("K ase");
+        sentenceQueue.Enqueue("Wasaaa");
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentenceQueue.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        dialogueText.text = sentenceQueue.Dequeue();
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+    }
+}
