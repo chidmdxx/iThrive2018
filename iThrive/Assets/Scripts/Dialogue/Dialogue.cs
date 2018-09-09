351dac011d4473a3cfe9eb5b7276dfab51f26be9
Zapata Ruiz <manruiz@amazon.com>
2018-09-09 15:27:28 -0700

    Conversation mechanics working

--- iThrive/Assets/Scripts/Dialogue/Dialogue.cs
+++ iThrive/Assets/Scripts/Dialogue/Dialogue.cs
@@ -10,4 +10,14 @@ public class Dialogue {
     public string reply2;
     public Dialogue option1;
     public Dialogue option2;
+
+    public static Dialogue dialogueGhost = new Dialogue
+        {
+            theirText = "",
+            myText = "",
+            reply1 = "I love you!",
+            reply2 = "Screw you!",
+            option1 = null,
+            option2 = null
+        };
 }
--- iThrive/Assets/Scripts/Dialogue/DialogueManager.cs
+++ iThrive/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -5,14 +5,19 @@ using UnityEngine.UI;
 public class DialogueManager : MonoBehaviour
 {
 
-    public Text dialogueTheirsText;
-    public Text dialogueMineText;
+    public Text dialogueTheirsRecentText;
+    public Text dialogueMineOldText;
+    public Text dialogueTheirsOldText;
+    public Text dialogueMineRecentText;
     public Text option1Text;
     public Text option2Text;
     public Text replyText;
     public Button option1Button;
     public Button option2Button;
-    public GameObject conversationMine;
+    public GameObject conversationMineOld;
+    public GameObject conversationMineRecent;
+    public GameObject conversationTheirsOld;
+    public GameObject conversationTheirsRecent;
 
     private Dialogue dialogue;
     private Dialogue dialogueGhost;
@@ -20,35 +25,19 @@ public class DialogueManager : MonoBehaviour
     private Dialogue dialogueFrank;
     private string currentCharacter;
     private int nextOption = -1;
+    private int ghostCount = 0;
 
     void Start()
     {
         currentCharacter = "Ghost";
-        dialogue = new Dialogue
-        {
-            theirText = "Want some coffee?",
-            myText = "",
-            reply1 = "Yeah",
-            reply2 = "Nah",
-            option1 = new Dialogue
-            {
-                theirText = "Yay!",
-                myText = "",
-                reply1 = "",
-                reply2 = ""
-            },
-            option2 = new Dialogue
-            {
-                theirText = "Bummer!",
-                myText = "",
-                reply1 = "",
-                reply2 = ""
-            }
-        };
+        dialogue = Dialogue.dialogueGhost;
 
-        conversationMine.gameObject.SetActive(false);
-        dialogueTheirsText.text = dialogue.theirText;
-        dialogueMineText.text = dialogue.myText;
+        conversationMineOld.gameObject.SetActive(false);
+        conversationMineRecent.gameObject.SetActive(false);
+        conversationTheirsOld.gameObject.SetActive(false);
+        conversationTheirsRecent.gameObject.SetActive(false);
+        dialogueTheirsRecentText.text = dialogue.theirText;
+        dialogueMineOldText.text = dialogue.myText;
         option1Text.text = dialogue.reply1;
         option2Text.text = dialogue.reply2;
         replyText.text = "";
@@ -57,14 +46,69 @@ public class DialogueManager : MonoBehaviour
     public void DisplayNextSentence()
     {
         SoundManager.Instance.PlaySingleSound("Click");
+
+        // Start Ghost hardcoded stuff
+        if (currentCharacter.Equals("Ghost"))
+        {
+            dialogue = Dialogue.dialogueGhost;
+            ghostCount++;
+            if (ghostCount > 10){
+                conversationMineOld.gameObject.SetActive(true);
+                conversationMineRecent.gameObject.SetActive(false);
+                conversationTheirsOld.gameObject.SetActive(false);
+                conversationTheirsRecent.gameObject.SetActive(true);
+                dialogueTheirsRecentText.text = "New phone who dis";
+                option1Text.text = "";
+                option2Text.text = "";
+                replyText.text = "";
+                nextOption = -1;
+                option1Button.gameObject.SetActive(false);
+                option2Button.gameObject.SetActive(false);
+                return;
+            }
+            else {
+                if (ghostCount !=1){
+                    conversationMineOld.gameObject.SetActive(true);
+                }
+                conversationMineRecent.gameObject.SetActive(true);
+                conversationTheirsOld.gameObject.SetActive(false);
+                conversationTheirsRecent.gameObject.SetActive(false);
+                option1Text.text = dialogue.reply1;
+                option2Text.text = dialogue.reply2;
+                replyText.text = "";
+            }
+
+            switch (nextOption)
+            {
+                case 1:
+                    dialogueMineOldText.text = dialogueMineRecentText.text;
+                    dialogueMineRecentText.text = dialogue.reply1;
+                    dialogue = Dialogue.dialogueGhost;
+                    break;
+                case 2:
+                    dialogueMineOldText.text = dialogueMineRecentText.text;
+                    dialogueMineRecentText.text = dialogue.reply2;
+                    dialogue = Dialogue.dialogueGhost;
+                    break;
+                default:
+                    return;
+            }
+            option1Button.gameObject.SetActive(true);
+            option2Button.gameObject.SetActive(true);
+            nextOption = -1;
+            return;
+        }
+
+        // End of Ghost hardcoded stuff
+
         switch (nextOption)
         {
             case 1:
-                dialogueMineText.text = dialogue.reply1;
+                dialogueMineOldText.text = dialogue.reply1;
                 dialogue = dialogue.option1;
                 break;
             case 2:
-                dialogueMineText.text = dialogue.reply2;
+                dialogueMineOldText.text = dialogue.reply2;
                 dialogue = dialogue.option2;
                 break;
             default:
@@ -77,11 +121,11 @@ public class DialogueManager : MonoBehaviour
             return;
         }
 
-        dialogueTheirsText.text = dialogue.theirText;
+        dialogueTheirsRecentText.text = dialogue.theirText;
         option1Text.text = dialogue.reply1;
         option2Text.text = dialogue.reply2;
         replyText.text = "";
-        conversationMine.gameObject.SetActive(true);
+        conversationMineOld.gameObject.SetActive(true);
         nextOption = -1;
     }
 
@@ -96,7 +140,7 @@ public class DialogueManager : MonoBehaviour
                 replyText.text = dialogue.reply1;
                 break;
             case 2:
-                replyText.text = dialogue.reply1;
+                replyText.text = dialogue.reply2;
                 break;
         }
         Debug.Log("Next option is " + option);
