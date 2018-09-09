e004fa4b8d1cb846ef78cf0e524d5d4d3c6918c0
Zapata Ruiz <manruiz@amazon.com>
2018-09-08 17:39:27 -0700

    Prefab for IM Window

---
+++ iThrive/Assets/Scripts/IM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using UnityEngine.UI;
+
+public class IM : Window {
+
+    public static readonly string WindowName = "IMWindow";
+
+    private Button closeButton;
+
+    protected override Action OnStart
+    {
+        get
+        {
+            return () =>
+            {
+                var textFields = this.GetComponentsInChildren<Text>();
+                this.IsCurrentWindow = true;
+            };
+        }
+    }
+
+    protected override Action OnUpdate
+    {
+        get
+        {
+            return () =>
+            {
+            };
+        }
+    }
+}
